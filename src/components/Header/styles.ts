7d7203bb9eb18608cfd12f7bb527e8d9d08d1b3b
Irys Salvartico <iryssalvatico16@gmail.com>
2021-09-11 14:38:50 -0300

    Projeto react

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components"
 
 export const Header = styled.header`
-    display: flex; 
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
     background-color: #E6B8B8;
 `
