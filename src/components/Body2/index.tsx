15e73901af430cb5ef9440a74ae75de923de5f4d
Irys Salvartico <iryssalvatico16@gmail.com>
2021-09-13 16:58:08 -0300

    Projeto react

--- src/components/Body2/index.tsx
+++ src/components/Body2/index.tsx
@@ -1,5 +1,10 @@
 import * as S from "./styles"
-
+import livro4 from "../../assets/LIVRO 4.svg"
+import livro5 from "../../assets/LIVRO 5.svg"
+import livro6 from "../../assets/LIVRO 6.svg"
+import livro7 from "../../assets/LIVRO 7.svg"
+import livro8 from "../../assets/LIVRO 8.svg"
+import r1 from "../../assets/Retangle 8.svg"
 
 const Body2 = () => {
     return (
@@ -10,11 +15,11 @@ const Body2 = () => {
             </section>
             <section className="livros">
 
-                <h1>livro 4</h1>  
-                <h1>livro 5</h1>  
-                <h1>livro 6</h1>  
-                <h1>livro 7</h1>  
-                <h1>livro 8</h1>  
+                <img  src = {livro4} alt="livro 4"/>  
+                <img  src = {livro5} alt="livro 5"/>  
+                <img  src=  {livro6} alt="livro 6"/>
+                <img  src = {livro7} alt="livro 7"/>  
+                <img  src = {livro8} alt="livro 8"/>  
                 
                  
             </section>
