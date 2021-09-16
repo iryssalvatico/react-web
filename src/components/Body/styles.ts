5952bb34590884bc48be3c115bec06a04d97dfd3
iryssalvatico <85744643+iryssalvatico@users.noreply.github.com>
2021-09-16 11:27:16 -0300

    Update styles.ts

--- src/components/Body/styles.ts
+++ src/components/Body/styles.ts
@@ -2,21 +2,26 @@ import styled from "styled-components"
 import bgPag1 from "../../assets/FUNDO.svg"
 
 export const Body = styled.body
+
+
     background-image: url("${bgPag1}");
     width: 100%;
     height: 50rem;
- nav{
+
+
+ nav {
+ 
     display: flex;
     flex-direction: column;
     align-items: center;
-    section p{
+    section p {
     margin: 2rem;
     background-color: #DAFAD3;
     text-align: center;
     font-size: 2rem;
     font-family: 'Old Standard TT';
     
-}
-}
+    }
+ }
   
-`
+  
